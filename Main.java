1c22e8406a0f3ba8b5a6a36eb8f1e8724a59b9e9
nishas2000 <141129284+nishas2000@users.noreply.github.com>
2023-08-03 11:44:03 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,53 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        //String name = "Mayuri";
+        //System.out.println(name);
+        //int value = name.length();
+        //System.out.println(value);
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring);
+
+       // String ustring = name.toUpperCase();
+        // System.out.println(ustring);
+
+        //String nonTrimmedString = "     Mayuri    ";
+        //System.out.println(nonTrimmedString);
+
+        //String trimmedString = nonTrimmedString.trim();
+        //System.out.println(trimmedString);
+
+        //name.substring(int start)
+        //System.out.println(name.substring(2));     //  Mayuri=uri(start from index 2)
+
+        //name.substring(int start,int end)
+        //System.out.println(name.substring(2,4));     //Mayuri= yu(start with index 2 and inds at index 4)
+
+        //name.replace()
+        //System.out.println(name.replace('r','l'));  //Mayuri replace by mayuli r gets replaced with l
+        //System.out.println(name.replace("y","dh"));   //use double cots
+
+        //name.Startswith()
+        //System.out.println(name.StartsWith(Ma));   //true
+        
+        //name.EndsWith()
+        //System.out.println(name,EndsWith(ri));     //true  
+        
+        //name.tochararry()
+        //System.out.println(name.toCharArray());     //converts string into sequence of characters  //Mayuri
+
+        //name.CharAt(int index )
+        //System.out.println(name.charAt(2));        //Mayuri=y index value
+
+        //getchar()
+
+        String str1 = "Mayuri";   //hashcode is 1
+        String str2 = "Mayuri";   //hashcode is 1
+
+       // string constant pool = all the strings are get stored in the scp .  it helps to save the memory.
+                                //  for security purpose
+
+
+    }
+}
