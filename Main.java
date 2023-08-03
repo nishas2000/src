39a6fd84b28c6bc8ea2109620812aaaaa1ae3825
saurabhbasve <102825775+saurabhbasve@users.noreply.github.com>
2023-08-03 13:34:32 +0530

    Update Main.java
    Read FIle have Modify  the content in SCP section

--- Main.java
+++ Main.java
@@ -42,12 +42,17 @@ public class Main {
 
         //getchar()
 
-        String str1 = "Mayuri";   //hashcode is 1
+        String str1 = "Mayuri";   //hashcode is 1 //This is StRing Literals 
         String str2 = "Mayuri";   //hashcode is 1
-
-       // string constant pool = all the strings are get stored in the scp .  it helps to save the memory.
-                                //  for security purpose
-
-
+        String str3 = new String("saurabh");   //This is using the object  It Wont go in the SCP it will go To the HEAP MEMORY as Object 
+       /* string constant pool = when we  create the string using the string literal in java then the string will go to the SCP .
+                                if we are creating the string which is already present in java program then java doesnot create the "new string"  again rather than 
+                                it will just refer that string which is present in the pool./*
+               //Reason behind this are the efficient memory managment for the JVM & as we use the string in large amount in the programming 
+    //Why string is immmutable in java ? /He vicharlyavr SCP pn sangun takaich 
+   // Security As the String is commonly utilized in Java applications. Immutable String objects are used to prevent making any more mistakes because they
+   //        load the right class.
+    //       Strings are used to hold sensitive data such as connection URLs, usernames, and passwords. Consider banking software: because String objects are immutable, any attacker will be unable to change the username and password. If Strings were mutable, we couldn't be sure whether the String we got was safe or not when running the update, even after performing security checks. 
+     //      Because of the immutability of strings in Java, the application software can be more secure.
     }
 }
